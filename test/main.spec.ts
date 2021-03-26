a765f41e83bb78cbce7fa90500e7054b00fa46c3
Konrad Baechler <konrad@diva.exchange>
2021-03-26 14:30:50 +0100

    adding unit test infrastructure (ts-node, mocha, chai, testdeck)

---
+++ test/main.spec.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2021 diva.exchange
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Author/Maintainer: Konrad Bächler <konrad@diva.exchange>
+ */
+
+import { suite, test } from '@testdeck/mocha';
+import chai from 'chai';
+
+import { P2P_NETWORK } from '../src/config';
+
+const assert = chai.assert;
+
+@suite
+class TestMain {
+
+  @test
+  network() {
+    assert.equal(Object.keys(P2P_NETWORK).length, 7);
+  }
+}
