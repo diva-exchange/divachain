118e3f37664964f9e9d82bc3ff00def4d309ccab
marta.jimenez <marta.jimenez@advertima.com>
2021-06-15 16:03:22 +0200

    Extended tests for config

--- test/config-logger.spec.ts
+++ test/config-logger.spec.ts
@@ -22,11 +22,22 @@ import { expect } from 'chai';
 
 import { Logger } from '../src/logger';
 import { Config, Configuration } from '../src/config';
+import fs from 'fs';
 
 @suite
 class TestConfigLogger {
   @test
   config() {
+    const c = new Config({ network_refresh_interval_ms: 5000, network_size: 100 } as Configuration);
+    expect(c.ip).is.not.empty;
+    expect(c.network_refresh_interval_ms).is.equal(5000);
+    expect(c.network_size).is.equal(64);
+  }
+
+  @test
+  configPkg() {
+    process['pkg'] = true;
+    process.execPath = __filename;
     const c = new Config({} as Configuration);
     expect(c.ip).is.not.empty;
   }
@@ -35,5 +46,21 @@ class TestConfigLogger {
   logger() {
     process.env.NODE_ENV = 'development';
     expect(Logger).is.not.undefined;
+  }
+
+  @test
+  configPathExist() {
+    fs.rmdirSync(__dirname + '/blockstore', { recursive: true });
+    fs.rmdirSync(__dirname + '/state', { recursive: true });
+    fs.rmdirSync(__dirname + '/keys', { recursive: true });
+    const c = new Config({ path_app: __dirname } as Configuration);
+    expect(c.ip).is.not.empty;
+  }
+
+  @test
+  configNetworkRefreshIntervalMs() {
+    const c = new Config({ path_app: __dirname, network_refresh_interval_ms: -1 } as Configuration);
+    expect(c.network_refresh_interval_ms).is.greaterThanOrEqual(1);
+    expect(c.network_refresh_interval_ms).is.equal(3000);
   }
 }
