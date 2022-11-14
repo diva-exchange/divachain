289274e2fb642634a8175eb31ecbb2e9c4389b2d
diva.exchange <61228890+diva-exchange@users.noreply.github.com>
2022-11-14 18:35:23 +0100

    fix: bash standards

--- bin/build.sh
+++ bin/build.sh
@@ -22,11 +22,11 @@
 set -e
 
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
-cd ${PROJECT_PATH}
-PROJECT_PATH=`pwd`/
+cd "${PROJECT_PATH}"
+PROJECT_PATH=$( pwd )
 
-source "${PROJECT_PATH}bin/echos.sh"
-source "${PROJECT_PATH}bin/helpers.sh"
+source "${PROJECT_PATH}"/bin/echos.sh
+source "${PROJECT_PATH}"/bin/helpers.sh
 
 if ! command_exists npm; then
   error "npm not available. Install node";
@@ -36,30 +36,30 @@ fi
 npm ci
 
 info "Clean up..."
-rm -rf ${PROJECT_PATH}dist/*
-rm -rf ${PROJECT_PATH}build/prebuilds
-rm -rf ${PROJECT_PATH}build/divachain-*
+rm -rf "${PROJECT_PATH}"/dist/*
+rm -rf "${PROJECT_PATH}"/build/prebuilds
+rm -rf "${PROJECT_PATH}"/build/divachain-*
 
 info "Transpiling TypeScript to JavaScript..."
-${PROJECT_PATH}node_modules/.bin/tsc -p ${PROJECT_PATH} --outDir ${PROJECT_PATH}dist
-rm -rf ${PROJECT_PATH}dist/version.js
-cp -r ${PROJECT_PATH}src/schema ${PROJECT_PATH}/dist/schema
+"${PROJECT_PATH}"/node_modules/.bin/tsc -p "${PROJECT_PATH}" --outDir "${PROJECT_PATH}"/dist
+rm -rf "${PROJECT_PATH}"/dist/version.js
+cp -r "${PROJECT_PATH}"/src/schema "${PROJECT_PATH}"/dist/schema
 
 # create a static version file
-${PROJECT_PATH}node_modules/.bin/ts-node --files ${PROJECT_PATH}src/version.ts
-cp ${PROJECT_PATH}build/version ${PROJECT_PATH}dist/version
+"${PROJECT_PATH}"/node_modules/.bin/ts-node --files "${PROJECT_PATH}"/src/version.ts
+cp "${PROJECT_PATH}"/build/version "${PROJECT_PATH}"/dist/version
 
 if command_exists pkg; then
   info "Packaging..."
 
   #classic-level prebuilds
-  [[ -d ${PROJECT_PATH}node_modules/classic-level/prebuilds/ ]] &&
-    mkdir -p ${PROJECT_PATH}build/prebuilds &&
-    cp -r ${PROJECT_PATH}node_modules/classic-level/prebuilds/linux-x64 ${PROJECT_PATH}build/prebuilds/linux-x64
+  [[ -d "${PROJECT_PATH}"/node_modules/classic-level/prebuilds/ ]] &&
+    mkdir -p "${PROJECT_PATH}"/build/prebuilds &&
+    cp -r "${PROJECT_PATH}"/node_modules/classic-level/prebuilds/linux-x64 "${PROJECT_PATH}"/build/prebuilds/linux-x64
 
   pkg --no-bytecode \
     --public \
-    --output ${PROJECT_PATH}build/divachain-linux-x64 \
+    --output "${PROJECT_PATH}"/build/divachain-linux-x64 \
     .
 else
   info "Skipping Packaging..."
